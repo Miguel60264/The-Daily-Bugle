e813d3967084aab35c7f691867f525a2757450d2
Miguel60264 <Miguelart2002@gmail.com>
2022-04-01 10:07:08 -0500

    Clase 1 de abril

---
+++ Happyland/Assets/_Scripts/PlayButtom.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtom : MonoBehaviour
+{
+    public string sceneToLoad;
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
